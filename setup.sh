3f78a29eb5a5d63f5707a7ea1b87ec81efd22eea
gerstle <gerstle@gmail.com>
2020-05-13 11:39:04 -0700

    minor updates

--- setup.sh
+++ setup.sh
@@ -24,6 +24,13 @@ done
 
 echo "installing asdf-vm..."
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+# asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+# asdf install ruby 2.6.2
+# asdf global ruby 2.6.2
+# asdf plugin-add java
+# asdf list all java
+# asdf install java amazon-corretto-11.0.6.10.1-2
+# asdf global java amazon-corretto-11.0.7.10.1
 
 source ~/.zshrc
 p10k configure
